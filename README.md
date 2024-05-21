b86b0bc0a12cadd8648b419b71fd82ddcfeaadbc
Romane Santos <romanesantos@gmail.com>
2024-05-21 20:31:14 -0300

    🎉 :: [Init] :: Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dev Links
+
+Repositorio contento a atividade final do curso Discover da Rocket Seat
+
+Projeto simples, servindo como agrupado de links estatico.
---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", () => {
+    let profileImageDark = "/assets/img/avatar.png", profileImageLight = "/assets/img/avatar-light.jpg";
+    let input = document.getElementById("change-theme");
+    let profileImage = document.querySelector("#profile-image img");
+    if (input) {
+        input.addEventListener("input", (e) => {
+            if (e.target.checked) {
+                document.body.dataset.theme = "light";
+                profileImage.src = profileImageLight;
+            } else {
+                document.body.dataset.theme = "dark";
+                profileImage.src = profileImageDark;
+            }
+        });
+    }
+});
